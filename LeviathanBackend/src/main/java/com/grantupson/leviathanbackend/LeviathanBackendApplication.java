6d35c9b03f8bca79878acd05fd6defe75b6c842b
Grant Upson <grant.upson@student.unimelb.edu.au>
2022-08-21 18:12:57 +1000

    Created initial Spring backend

---
+++ LeviathanBackend/src/main/java/com/grantupson/leviathanbackend/LeviathanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grantupson.leviathanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeviathanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeviathanBackendApplication.class, args);
+    }
+
+}
---
+++ LeviathanBackend/src/test/java/com/grantupson/leviathanbackend/LeviathanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grantupson.leviathanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeviathanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
